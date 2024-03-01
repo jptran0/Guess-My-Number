0543227ce84bbff15ef1a0ac3187eb85e077f921
Jazmine Tran <jazminetra00@gmail.com>
2024-03-01 14:40:32 -0600

    added high score

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,6 +23,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // too high
   } else if (guess > secretNumber) {
