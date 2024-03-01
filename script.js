ea3398c0c5196e5865ab69130966ed11f6d2a3c7
Jazmine Tran <jazminetra00@gmail.com>
2024-03-01 13:05:16 -0600

    initialized variables and guess if statement

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+'use strict';
 
+// to get a class use '.'
+// to get an id use '#'
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+
+    // player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
+  }
+});
